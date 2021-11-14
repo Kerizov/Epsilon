2298591d7bdecf9d189f8171f93e89bd2bb29076
Kerizov <stepanok980@gmail.com>
2021-11-14 23:00:40 +0300

    EpsilovV4

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,25 @@ window.onload = function(){
     $('.load').remove();
 
 }
+
+let menu_btn = document.querySelector(".mob-menu_btn");
+let menu_btn_close = document.querySelector(".close_btn");
+let mob_menu = document.querySelector(".mob__menu");
+function mobileMenu(){
+
+    menu_btn.addEventListener("click",function(){
+        menu_btn.style.display = "none";
+        menu_btn_close.style.display = "block";
+        mob_menu.style.display = "flex";
+
+    });
+
+    menu_btn_close.addEventListener("click", function(){
+        menu_btn.style.display = "block";
+        menu_btn_close.style.display = "none";
+        mob_menu.style.display = "none";
+
+    });
+}
+
+mobileMenu();
